08c412c0a20bb460815621928887529e7d8a28fd
msaswata15 <121812665+msaswata15@users.noreply.github.com>
2025-08-04 17:20:03 +0530

    Initial Files Uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
 function App() {
+  let [counter, setCounter] = useState(0);
+  const addValue = () => {
+    setCounter((prevCounter) =>
+      prevCounter + 2
+    );
+    setCounter((prevCounter) =>
+      prevCounter + 2
+    );
+    setCounter((prevCounter) =>
+      prevCounter + 2
+    );
+    setCounter((prevCounter) =>
+      prevCounter + 2
+    );
+  }
+
+
+  const subtractValue = () => {
+    setCounter(counter - 2);
+  }
+  const resetValue = () => {
+    setCounter(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold'>Hello World</h1>
+      <h2 className='text-xl'>Counter Value: {counter}</h2>
+      <div className="flex items-center justify-center">
+        <button onClick={addValue} className='m-2 bg-blue-500 p-3 rounded-3xl'>
+          Increment
+        </button>
+        <button onClick={subtractValue} className='m-2 bg-red-500 p-3 rounded-3xl'>Decrement</button>
+        <button onClick={resetValue} className='m-2 bg-gray-500 p-3 rounded-3xl'>Reset</button>
+      </div></div >
   );
 }
 
